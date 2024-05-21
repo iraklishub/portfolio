6b225346c5cc80fa40122a5d4b2ca7811df91734
iraklishub <51046973+iraklishub@users.noreply.github.com>
2024-05-21 22:44:55 +0400

    disable contact submit btn for now

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -46,29 +46,31 @@ const Contact = () => {
 
         <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
+            <span className="text-white font-medium mb-4">Your Name *</span>
             <input
               type="text"
               name="name"
               value={form.name}
               onChange={handleChange}
+              required={true}
               placeholder="What's your good name?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your email</span>
+            <span className="text-white font-medium mb-4">Your email *</span>
             <input
               type="email"
               name="email"
               value={form.email}
               onChange={handleChange}
+              required={true}
               placeholder="What's your web address?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Message</span>
+            <span className="text-white font-medium mb-4">Your Message *</span>
             <textarea
               rows={7}
               name="message"
@@ -82,6 +84,7 @@ const Contact = () => {
           <button
             type="submit"
             className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+            disabled
           >
             {loading ? 'Sending...' : 'Send'}
           </button>
